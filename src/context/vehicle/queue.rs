dcd86dbf6196714448265715700bafa672b97bc7
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2023-03-15 00:10:44 +0000

    maintain safe distance

--- src/context/vehicle/queue.rs
+++ src/context/vehicle/queue.rs
@@ -20,11 +20,21 @@ impl Queue {
     pub fn create_vehicle(&mut self, origin: Origin, id: i32) {
         let vehicle_direction = VehicleDirection::random();
         let vehicle = RefCell::new(Vehicle::new(origin.clone(), &vehicle_direction, id));
-        match origin {
-            Origin::East => vehicle_direction.push_to_vehicle_direction(&mut self.east, vehicle),
-            Origin::West => vehicle_direction.push_to_vehicle_direction(&mut self.west, vehicle),
-            Origin::North => vehicle_direction.push_to_vehicle_direction(&mut self.north, vehicle),
-            Origin::South => vehicle_direction.push_to_vehicle_direction(&mut self.south, vehicle),
+        if self.check_c_q(&origin, &vehicle_direction) {
+            match origin {
+                Origin::East => {
+                    vehicle_direction.push_to_vehicle_direction(&mut self.east, vehicle)
+                }
+                Origin::West => {
+                    vehicle_direction.push_to_vehicle_direction(&mut self.west, vehicle)
+                }
+                Origin::North => {
+                    vehicle_direction.push_to_vehicle_direction(&mut self.north, vehicle)
+                }
+                Origin::South => {
+                    vehicle_direction.push_to_vehicle_direction(&mut self.south, vehicle)
+                }
+            }
         }
     }
     pub fn remove_first_in_queue(
@@ -37,6 +47,102 @@ impl Queue {
             Origin::West => self.west.remove_first_from_direction(vehicle_direction),
             Origin::North => self.north.remove_first_from_direction(vehicle_direction),
             Origin::South => self.south.remove_first_from_direction(vehicle_direction),
+        }
+    }
+    pub fn check_c_q(&self, origin: &Origin, vehicle_direction: &VehicleDirection) -> bool {
+        match origin {
+            Origin::East => match vehicle_direction {
+                VehicleDirection::Left => {
+                    if let Some(val) = self.east.left.last() {
+                        val.borrow().point.0 < 560
+                    } else {
+                        true
+                    }
+                }
+                VehicleDirection::Straight => {
+                    if let Some(val) = self.east.straight.last() {
+                        val.borrow().point.0 < 560
+                    } else {
+                        true
+                    }
+                }
+                VehicleDirection::Right => {
+                    if let Some(val) = self.east.right.last() {
+                        val.borrow().point.0 < 560
+                    } else {
+                        true
+                    }
+                }
+            },
+            Origin::West => match vehicle_direction {
+                VehicleDirection::Left => {
+                    if let Some(val) = self.west.left.last() {
+                        val.borrow().point.0 > 20
+                    } else {
+                        true
+                    }
+                }
+                VehicleDirection::Straight => {
+                    if let Some(val) = self.west.straight.last() {
+                        val.borrow().point.0 > 20
+                    } else {
+                        true
+                    }
+                }
+                VehicleDirection::Right => {
+                    if let Some(val) = self.west.right.last() {
+                        val.borrow().point.0 > 20
+                    } else {
+                        true
+                    }
+                }
+            },
+            Origin::North => match vehicle_direction {
+                VehicleDirection::Left => {
+                    if let Some(val) = self.north.left.last() {
+                        val.borrow().point.1 > 20
+                    } else {
+                        true
+                    }
+                }
+                VehicleDirection::Straight => {
+                    if let Some(val) = self.north.straight.last() {
+                        val.borrow().point.1 > 20
+                    } else {
+                        true
+                    }
+                }
+                VehicleDirection::Right => {
+                    if let Some(val) = self.north.right.last() {
+                        val.borrow().point.1 > 20
+                    } else {
+                        true
+                    }
+                }
+            },
+            Origin::South => match vehicle_direction {
+                VehicleDirection::Left => {
+                    if let Some(val) = self.south.left.last() {
+                        val.borrow().point.1 < 560
+                    } else {
+                        true
+                    }
+                }
+                VehicleDirection::Straight => {
+                    if let Some(val) = self.south.straight.last() {
+                        val.borrow().point.1 < 560
+                    } else {
+                        true
+                    }
+                }
+                VehicleDirection::Right => {
+                    if let Some(val) = self.south.right.last() {
+                        val.borrow().point.1 < 560
+                    } else {
+                        true
+                    }
+                }
+            },
         }
     }
 }
